7a921f70a4e6e9254298648df5c164169bfd1960
Aleksandar Travlev <ATravlev@nemetschek.bg>
2018-10-05 08:56:47 +0000

    create new form class

--- public/js/app.js
+++ public/js/app.js
@@ -51,7 +51,133 @@ class Errors {
      * @param {string|null} field
      */
     clear(field) {
-        delete this.errors[field];
+        if(field) {
+            delete this.errors[field];
+            return;
+        }
+
+        this.errors = {};
+    }
+}
+
+class Form {
+
+    /**
+     * Create a new Form instance.
+     *
+     * @param {object} data
+     */
+    constructor(data) {
+        this.originalData = data;
+
+        for (let field in data) {
+            this[field] = data[field];
+        }
+
+        this.errors = new Errors();
+    }
+
+    /**
+     * Fetch all relevant data for the form.
+     */
+    data() {
+        let data = {};
+
+        for (let property in this.originalData) {
+            data[property] = this[property];
+        }
+
+        return data;
+    }
+
+    /**
+     * Reset the form fields.
+     */
+    reset() {
+        for (let field in this.originalData) {
+            this[field] = '';
+        }
+
+        this.errors.clear();
+    }
+
+    /**
+     * Send a POST request to the given URL.
+     * .
+     * @param {string} url
+     */
+    post(url) {
+        return this.submit('post', url);
+    }
+
+    /**
+     * Send a PUT request to the given URL.
+     * .
+     * @param {string} url
+     */
+    put(url) {
+        return this.submit('put', url);
+    }
+
+    /**
+     * Send a PATCH request to the given URL.
+     * .
+     * @param {string} url
+     */
+    patch(url) {
+        return this.submit('patch', url);
+    }
+
+    /**
+     * Send a DELETE request to the given URL.
+     * .
+     * @param {string} url
+     */
+    delete(url) {
+        return this.submit('delete', url);
+    }
+
+    /**
+     * Submit the form.
+     *
+     * @param {string} requestType
+     * @param {string} url
+     */
+    submit(requestType, url) {
+
+        return new Promise((resolve, reject) => {
+            axios[requestType](url, this.data())
+                .then(response => {
+                    this.onSuccess(response.data);
+
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    this.onFail(error.response.data.errors);
+
+                    reject(error.response.data.errors);
+                });
+        });
+        
+    }
+
+    /**
+     * Handle a successful form submission.
+     *
+     * @param {object} data
+     */
+    onSuccess(data) {
+        alert(data.message);
+        this.reset();
+    }
+
+     /**
+     * Handle a failed form submission.
+     *
+     * @param {object} errors
+     */
+    onFail(errors) {
+        this.errors.record(errors);
     }
 }
 
@@ -60,16 +186,15 @@ new Vue({
     el: '#app',
 
     data: {
-        name: '',
-        description: '',
-        errors: new Errors(),
+        form: new Form({
+            name: '',
+            description: ''
+        }),
     },
 
     methods: {
         onSubmit() {
-            axios.post('/projects', this.$data)
-                .then(response => alert('Success'))
-                .catch(error => this.errors.record(error.response.data.errors));
+            this.form.post('/projects');
         }
     }
 
