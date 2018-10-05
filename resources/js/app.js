023ba70b54ca146951e023e4b8eb87686d875caa
Aleksandar Travlev <ATravlev@nemetschek.bg>
2018-10-05 09:58:02 +0000

    webpack config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,24 @@
+import Vue from 'vue';
+import axios from 'axios';
+import Form from './core/Form';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+window.axios = axios;
+window.Form = Form;
 
-require('./bootstrap');
+new Vue({
+    el: '#app',
 
-window.Vue = require('vue');
+    data: {
+        form: new Form({
+            name: '',
+            description: ''
+        }),
+    },
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+    methods: {
+        onSubmit() {
+            this.form.post('/projects');
+        }
+    }
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
 });
